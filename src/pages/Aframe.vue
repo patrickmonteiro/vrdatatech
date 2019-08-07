9d59f2c86272c5b196a92444023f9ab77a22032f
patryckx <patrick.monteiro@w3as.com>
2019-08-07 19:08:39 -0300

    First Commit :tada:

---
+++ src/pages/Aframe.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="container scene">
+    <a-scene arjs='debugUIEnabled: false;'>
+      <!-- <a-marker id='frac3' type='pattern' url='statics/marker/pt-frac3.patt' vidhandler>
+        <a-entity geometry="primitive: circle; radius: 1" rotation="-90 0 0"
+          scale="1.8 1.5 1.5" material="shader:gif;src:url(statics/india.gif);"
+          gif=""></a-entity>
+      </a-marker> -->
+      <a-marker id='vid' type='pattern' url='statics/marker/pt-frac3.patt'>
+            <a-plane src="statics/logoDataTech.png" scale="3.3 3.3 3.3" rotation="-90 0 0" position="0 0 0">
+            </a-plane>
+
+            <!-- <a-plane color="white" width="5" position="0 0 0" rotation="-90 0 0"></a-plane>
+            <a-text value="Obra: Quadrado Magico Antonio Peticov" align="center" color="black" width="5"
+                position="0 0.8 -1" rotation="-90 0 0"></a-text> -->
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Aframe'
+}
+</script>
+
+<style>
+.scene {
+    z-index: 100 !important;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="statics/logoDataTech.png" width="250rem">
+    <q-btn color="purple-8"
+      class="full-width q-mt-md"
+      label="Iniciar Realidade Aumentada"
+      @click="routeModify()"/>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods: {
+    routeModify () {
+      this.$router.push({ name: 'aframe' })
+    }
+  }
+}
+</script>
